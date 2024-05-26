adaec9640b22f95c2d0dfe6300b6f663007b0b08
Jess0419 <jmaciacochran@hawkmail.hccfl.edu>
2024-05-26 20:42:57 +0000

    Unit Test_Assignment 2-4

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 # input statements
-salary = float(input())
-numDependents = float(input())
+salary = 30000
+numDependents = 6
 
 # calculate taxes here
-
+stateTax = salary * 0.065
+federalTax = salary * 0.28
+dependentDeduction = (salary * 0.025) * numDependents
+totalWithholding = stateTax + federalTax + dependentDeduction
+takeHomePay = salary - totalWithholding
 # output statements
+print("State Tax: $" + str(stateTax))
+print("Federal Tax: $" + str(federalTax))
+print("Dependents: $" + str(dependentDeduction))
 print("Salary: $" + str(salary))
 print("Take Home Pay: $" + str(takeHomePay))
